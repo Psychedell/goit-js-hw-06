192af7fbde4e7368244ce85a0c5bbf809fa9d27d
Oleg Kaminskyy <olegkaminskyy@gmail.com>
2022-07-19 18:56:14 +0300

    task 3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,21 @@ const images = [
     alt: "Group of Horses Running",
   },
 ];
+
+const listEl = document.querySelector(".gallery");
+
+const addListEl = images
+  .map(
+    ({ url, alt }) =>
+      `<li><img class="list_image" src=${url} alt=${alt} "></li>`
+  )
+  .join("");
+
+listEl.insertAdjacentHTML("afterbegin", addListEl);
+
+// listEl.innerHTML = `<li><img src="https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+// alt="White and Black Long Fur Cat" style="width: 500px"></li>
+// <li><img src="https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+// alt="Orange and White Koi Fish Near Yellow Koi Fish" style="width: 500px"></li>
+// <li><img src="https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+// alt="Group of Horses Running" style="width: 500px"></li>`;
