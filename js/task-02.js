b3f1e9623d8506646203bdc2314343c9a89be1fc
Oleg Kaminskyy <olegkaminskyy@gmail.com>
2022-07-19 17:22:18 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,26 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const newItemsEl = document.querySelector("#ingredients");
+
+const arrElements = ingredients.map((element) => {
+  const itemEl = document.createElement("li");
+  itemEl.classList.add("item");
+  itemEl.textContent = element;
+  return itemEl;
+});
+
+newItemsEl.append(...arrElements);
+
+// const itemEl = document.createElement("li");
+// itemEl.classList.add("item");
+// itemEl.textContent = ingredients[0];
+// console.log(itemEl);
+
+// newItemsEl.append(itemEl);
