e49d1839ed17740e6aa931357f6cb14e258ed244
Oleg Kaminskyy <olegkaminskyy@gmail.com>
2022-07-22 16:37:31 +0300

    task 8 start

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(element) {
+  element.preventDefault();
+  const {
+    elements: { email, password },
+  } = element.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return console.log("Please fill in all fields!");
+  }
+
+  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  element.currentTarget.reset();
+}
