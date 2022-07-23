0e7ec2bc9dbfde1b6319f051fda36c2c906f27bc
Oleg Kaminskyy <olegkaminskyy@gmail.com>
2022-07-23 13:19:18 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const divEl = document.querySelector("div.widget");
+const colorSpanEl = document.querySelector("span.color");
+const colorBattonEl = document.querySelector("button.change-color");
+
+colorBattonEl.addEventListener("click", oncolorBattonClick);
+
+function oncolorBattonClick() {
+  colorSpanEl.textContent = getRandomHexColor();
+  divEl.style.backgroundColor = colorSpanEl.textContent;
+}
